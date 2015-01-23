76d35fdc91b2a0e5bf3b2ff641dcc3aadb5d0bcd
kms70847 <kevin.m.smiley@gmail.com>
2015-01-23 08:14:05 -0500

    adhering to pep8 style guidelines

--- src/lex.py
+++ src/lex.py
@@ -1,57 +1,66 @@
-#subclasses should implement:
-#attribute `self.name`
-#method `self.match(s:str)`, which returns either `None` if the string does not match, or the length of the slice of the string that matches.
+import re
+
+
+# subclasses should implement:
+# attribute `self.name`
+# method `self.match(s:str)`, which returns either `None` if the string does not match, or the length of the slice of the string that matches.
 class TokenRule:
     pass
 
-import re
-    
+
 class RegexTokenRule(TokenRule):
     def __init__(self, name, regex):
         self.name = name
         self.regex_text = regex
         self.regex = re.compile(regex)
+
     def match(self, s):
         result = self.regex.match(s)
-        if not result: return None
+        if not result:
+            return None
         return len(result.group(0))
+
     def __repr__(self):
         return "RegexTokenRule({}, {})".format(repr(self.name), repr(self.regex_text))
+
 
 class LiteralTokenRule(TokenRule):
     def __init__(self, name):
         self.name = name
+
     def match(self, s):
         if s.startswith(self.name):
             return len(self.name)
         else:
             return None
+
     def __repr__(self):
         return "LiteralTokenRule({})".format(repr(self.name))
 
+
 def gen_token_rules(text, rules):
     from primitives import Terminal
-    #identify terminal symbols in our language
+    # identify terminal symbols in our language
     terminals = list(set(symbol.value for rule in rules for symbol in rule.RHS if isinstance(symbol, Terminal)))
-    
+
     lines = text.split("\n")
     lines = [line.strip() for line in lines if len(line.strip()) > 0 and not line.startswith("#")]
 
-    #pass 1: determine which terminals are defined in the tokens file
+    # pass 1: determine which terminals are defined in the tokens file
     nonliteral_tokens = set()
     for line in lines:
         nonliteral_tokens.add(line.partition("->")[0].strip())
     literal_tokens = list(set(terminals) - nonliteral_tokens)
     nonliteral_tokens = list(nonliteral_tokens)
-    
+
     rules = []
-    #literals should have higher priority than nonliterals.
-    #this is generally a good idea since nonliterals tend to match much more than literals,
-    #and they would overshadow literals if they had higher priority.
+    # literals should have higher priority than nonliterals.
+    # this is generally a good idea since nonliterals tend to match much more than literals,
+    # and they would overshadow literals if they had higher priority.
     for literal in literal_tokens:
         rules.append(LiteralTokenRule(literal))
-    
-    #pass 2: compiling nonliterals
+
+    # pass 2: compiling nonliterals
     for line in lines:
         name, _, regex = line.partition("->")
         name = name.strip()
@@ -60,13 +69,16 @@ def gen_token_rules(text, rules):
 
     return rules
 
+
 class Token:
     def __init__(self, klass, value, position=None):
         self.klass = klass
         self.value = value
         self.position = position
+
     def __repr__(self):
         return "Token({}, {}, {})".format(repr(self.klass.name), repr(self.value), self.position)
+
 
 def lex(text, token_rules):
     idx = 0
