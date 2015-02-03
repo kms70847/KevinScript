b7ab33bf656544047974cd09c1f1bfcedee24b49
kms70847 <kevin.m.smiley@gmail.com>
2015-02-02 19:51:25 -0500

    Load language.txt and tokens.txt from `src`, not current working directory

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 # ugly stuff to import modules one directory up
 import os
 import sys
-top_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+top_dir = os.path.dirname(cur_dir)
 parser_dir = os.path.join(top_dir, "lib", "parser")
 sys.path.insert(0, parser_dir)
-
 import ast
 from eval_ast import evaluate
 from kobjects import builtins
@@ -14,7 +14,12 @@ if len(sys.argv) < 2:
     print("please supply a file name.")
     sys.exit(0)
 reducible_nodes = ["StatementList", "ExpressionList", "KeyValueList"]
-tree = ast.construct_ast("tokens.txt", "language.txt", sys.argv[1], reducible_nodes)
+tree = ast.construct_ast(
+    os.path.join(cur_dir, "tokens.txt"), 
+    os.path.join(cur_dir, "language.txt"), 
+    sys.argv[1], 
+    reducible_nodes
+)
 
 scopes = [builtins]
 evaluate(tree, scopes)
