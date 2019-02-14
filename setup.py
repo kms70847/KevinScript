97be5ec9e4cd9cedae6d4cee38998050337f3094
Kevin <kevin.m.smiley@gmail.com>
2019-02-14 14:34:36 -0500

    Include parser setup files and builtin initialization when packaging

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='KevinScript',
     version='1.0.0',
     packages=find_packages(),
+    data_files=[('ks', ['ks/language.txt', 'ks/tokens.txt', 'ks/native_builtin_initialization.k'])],
     entry_points={
         'console_scripts': [
             'kevinscript=ks:main'
